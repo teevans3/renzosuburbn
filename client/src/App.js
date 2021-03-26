da00999973c5a667866712f56b8bdfb4cb6372ab
Thomas Evans <thomasevans@Thomass-MBP.attlocal.net>
2021-03-26 17:12:39 -0400

    still wroking on it

--- client/src/App.js
+++ client/src/App.js
@@ -93,6 +93,7 @@ function App() {
       // TODO
       console.log(err);
     })
+
   }, [])
 
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const controllers = require('./../controllers/controllers');
 
-router.get('/postTwitch', controllers.postTwitch);
+router.post('/postTwitch', controllers.postTwitch);
 router.post('/postEmail', controllers.postEmail);
 
 module.exports = router;
